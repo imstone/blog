4e9dd4c482b9d46dcb913934d476317ff3494689
tanruixing <trxx@163.com>
2016-07-10 23:20:08 +0800

    增加路由

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ module.exports = function (app) {
 	app.get('/w', function(req, res, next) {
 	  res.render('index', { title: 'dd' });
 	});
+	app.get('/d', function(req, res, next) {
+	  res.render('index', { title: 'dd' });
+	});
 	app.get('/*', function(req, res, next) {
 	  res.send('sss')
 	});
