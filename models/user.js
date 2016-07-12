1e0cc85f178b74f853290d8784f9b83e1ee0da8c
tanruixing <trxx@163.com>
2016-07-12 23:59:13 +0800

    注册登录

---
+++ models/user.js
@@ -0,0 +1,37 @@
+var mongodb = require('./db');
+function User(user){
+    this.name = user.name;
+    this.password = user.password;
+    this.email = user.email;
+}
+
+module.exports = User;
+
+User.prototype.save = function(callback) {
+    var user = {
+        name: this.name,
+        password: this.password,
+        email: this.email
+    };
+    mongodb.open(function(err, db){
+        if(!err){
+            console.log('connect db');
+            db.collection('users',{safe:true}, function(err, collection){
+                if (err) {
+                    mongodb.close();
+                    return callback(err);
+                }
+                collection.insert(user,{safe:true},function(err, result){
+                    mongodb.close();
+                    if (err) {
+                        mongodb.close();
+                        return callback(err);
+                    }
+                    callback(null, user[0])
+                }); 
+            })
+        }else{
+            return callback(err)
+        }
+    })
+};
